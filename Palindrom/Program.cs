5671a0820b3cddb7c59be2b39eed5fde4d971e47
Ebubekir Yeniçağ <bekiryenicag06@gmail.com>
2024-12-10 20:47:07 +0300

    Add files via upload

---
+++ Palindrom/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("metni gir");
+string world=Console.ReadLine();
+
+string bossuz="";
+for (int i = 0; i < world.Length; i++)
+{
+    if ( world[i] != ' ')
+    {
+        bossuz+=world[i];
+    }
+}
+
+string tersbossuz="";
+foreach (var item in bossuz)
+{
+    tersbossuz=item+tersbossuz;
+}
+
+if (tersbossuz==bossuz)
+{
+    Console.WriteLine("Girdiginiz metin palindromdur");
+}
+    else 
+    {
+        Console.WriteLine("girilen metin palindrom degildir !!!! ");
+
+    }
