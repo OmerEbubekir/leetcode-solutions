d7b5c1c96bb420e15b2bf9ec70e2713a25eab44b
Ebubekir Yeniçağ <bekiryenicag06@gmail.com>
2024-12-10 10:57:29 +0300

    Add files via upload

---
+++ ContainsDupLicate/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] nums = [1,1,1,3,3,4,3,2,4,2];
+int counter=0;
+
+for (int i = 0; i < nums.Length; i++)
+{
+    for (int k = 0; k < nums.Length; k++)
+    {
+        if (nums[i]==nums[k])
+        {
+            counter++;
+        }
+    }
+}
+counter=counter-nums.Length;
+if (counter==0)
+{
+    Console.Write("false");
+}
+
+else
+{
+    Console.Write("True");
+}
