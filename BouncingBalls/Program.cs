de4ce2b8431d5af28ae8241260ad8cffe04c9fb1
Ebubekir Yeniçağ <bekiryenicag06@gmail.com>
2024-12-11 12:43:37 +0300

    Add files via upload

---
+++ BouncingBalls/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter the starting height of the ball:");
+double h = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("Enter the bounce factor (between 0 and 1):");
+double bounce = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the height of the mom's window:");
+double window = Convert.ToDouble(Console.ReadLine());
+
+ int counter = 0;
+if (h > 0 && bounce > 0 && bounce < 1 && window > 0)    // Validity checks
+
+{
+    if (h>window)
+    {
+    
+        for ( ; h> window  ;counter++)
+        {
+            h=h*bounce;
+    
+            if (h>window)
+            {
+            counter++;
+
+            }
+        }
+    }
+}
+else
+{
+    counter=-1;
+
+}
+
+Console.WriteLine($"The ball is seen {counter} times.");
