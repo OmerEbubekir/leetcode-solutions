ac76ad84165e9114c638d03bdefc88c090125ba6
Ebubekir Yeniçağ <bekiryenicag06@gmail.com>
2024-12-14 13:30:03 +0300

    Add files via upload

---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter the numbers");
+string x=Convert.ToString(Console.ReadLine());
+string xnegatifse="";
+string outp="";
+if (x.StartsWith('-'))
+{
+    xnegatifse=x.Replace('-',' ');
+
+    for (int i = x.Length-1; i >=0; i--)
+    {
+    
+        outp=outp+xnegatifse[i];
+
+    }
+        Console.WriteLine("-"+outp);
+}
+if (!(x.StartsWith('-')))
+{
+    
+    for (int i = x.Length-1; i >=0; i--)
+    {
+        
+        outp=outp+x[i];
+
+    }
+        Console.WriteLine(outp);
+}
