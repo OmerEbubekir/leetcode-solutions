75e64a82f9b0c8dca6594cb50c773f02f93662b6
Ebubekir Yeniçağ <bekiryenicag06@gmail.com>
2024-12-14 13:28:22 +0300

    Add files via upload

---
+++ LongestSubstringWithoutRepeatingCharcters/Program.cs
@@ -0,0 +1,36 @@
+﻿string s = "abcabcbb";
+int[] outp =new int[s.Length];
+char ilkKarakter = s[0];
+bool hepsiAyni = true;
+
+foreach (char karakter in s)
+{
+    if (karakter != ilkKarakter)
+    {
+        hepsiAyni = false;
+        break;
+    }
+}
+
+for (int i = 0; i < s.Length; i++)
+{
+    int counter=0;
+    for (int k = 0; k < s.Length; k++)
+    {
+        if (s[i]!=s[k])
+        {
+            counter++;
+         //Console.WriteLine("aa"+counter);
+
+        }
+        if (s[i]==s[k])
+        {
+            counter--;
+            //Console.WriteLine("bbb"+counter);
+        }
+      
+    }
+    outp[i]=counter;
+}
+Console.WriteLine(hepsiAyni ? "Bütün elemanlar aynı. 1" 
+: "Elemanlar farklı."+(outp.Max()-1));
