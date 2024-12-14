5b167feca0896fb0a90b75bcff0aeff5f3392da7
Ebubekir Yeniçağ <bekiryenicag06@gmail.com>
2024-12-14 13:27:34 +0300

    Add files via upload

---
+++ ContainerWithMostWater/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] height = [1,8,6,2,5,4,8,3,7];
+int outp=0;
+
+for (int i = 0; i < height.Length-1; i++)
+{   
+    int container=0;        // alan degişkeni
+    for (int j = height.Length-1; j >=0; j--)
+    {
+        if (height[i]<=height[j])
+        {
+            container=height[i]*(j-i);  //indexler arası mesafe carpı kücük olan degişken cümkü su kücüge göre dolar
+        }
+        else if (height[j]<=height[i])
+        {
+            container=height[j]*(j-i);
+        }
+
+        if (outp<container)     // kontrol esnasında eger alan bir öncekinden büyükse onu en büyük ata
+        {
+            outp=container;
+        }
+        
+    }
+}
+Console.WriteLine("Max Container"+outp);
